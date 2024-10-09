4990f1bd381f565e2d11172b8ea708811fafc68d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-10-09 11:22:54 +0100

    fix: corrected package exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     target: 'node18',
     outDir: 'dist',
     clean: true,
+    dts: true,
+    format: ['cjs', 'esm'],
+    sourcemap: true,
     minify: process.env.NODE_ENV === 'production',
     esbuildOptions: (opts) => {
       opts.resolveExtensions = ['.ts', '.mjs', '.js'];
